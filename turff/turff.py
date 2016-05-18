31b545a06d74b51ce6e7bfd11caad8949ac4c841
Todor Minchev <todor.minchev@linux.intel.com>
2016-05-18 14:39:08 -0700

    turff: add docstrings to Turff methods
    Signed-off-by: Todor Minchev <todor.minchev@linux.intel.com>

--- turff/turff.py
+++ turff/turff.py
@@ -17,9 +17,13 @@ from flask import json
 import requests
 
 class Turff():
-    '''Turff is a toolchain description utility
-    '''
+    '''Turff is a toolchain description utility'''
+
     def load_json(self, j_file):
+        '''Load JSON toolchain descriptor
+        j_file: JSON file name
+        returns: JSON object
+        '''
         try:
             with open(j_file) as json_data:
                 return json.load(json_data)
@@ -27,5 +31,10 @@ class Turff():
             return None
 
     def send_registration(self, url, j_data):
+        '''Send JSON toolchain registration to codi
+        url: codi toolchain registration URL
+        j_data: JSON object
+        returns: JSON object
+        '''
         return requests.post(url, json=j_data)
 
